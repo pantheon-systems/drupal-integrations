8b2d68cd5c9e3b4997e4013adc88a3264376787b
Greg Anderson <greg.1.anderson@greenknowe.org>
2020-01-30 09:03:22 -0800

    Don't skip permissions hardening by default.

--- assets/initial.settings.php
+++ assets/initial.settings.php
@@ -17,6 +17,15 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 include __DIR__ . "/settings.pantheon.php";
 
 /**
+ * Skipping permissions hardening will make scaffolding
+ * work better, but will also raise a warning when you
+ * install Drupal.
+ *
+ * https://www.drupal.org/project/drupal/issues/3091285
+ */
+// $settings['skip_permissions_hardening'] = TRUE;
+
+/**
  * If there is a local settings file, then include it
  */
 $local_settings = __DIR__ . "/settings.local.php";
--- assets/settings.pantheon.php
+++ assets/settings.pantheon.php
@@ -194,15 +194,3 @@ if (empty($settings['file_scan_ignore_directories'])) {
     'bower_components',
   ];
 }
-
-/**
- * Drupal attempts to change the permissions of sites/default/settings.php
- * and similar files for security purposes. This hardening is not necessary
- * on Pantheon, and can result in problems with scaffolding.
- *
- * https://www.drupal.org/project/drupal/issues/3091285
- *
- */
-if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
-  $settings['skip_permissions_hardening'] = TRUE;
-}
