1dda5918582badd9c563bf0b343c50a3b98e04a9
Greg Anderson <greg@getpantheon.com>
2020-05-11 11:24:30 -0700

    Uniform config directory
    Do not support both the legacy and the recommended layout in this project; only support recommended (relocated docroot).

--- assets/settings.pantheon.php
+++ assets/settings.pantheon.php
@@ -67,7 +67,7 @@ if ($is_installer_url) {
   $settings['config_sync_directory'] = 'sites/default/files';
 }
 else {
-  $settings['config_sync_directory'] = getenv('DOCROOT') ? '../config' : 'sites/default/config';
+  $settings['config_sync_directory'] = '../config';
 }
 
 
