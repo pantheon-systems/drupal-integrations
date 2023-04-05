c6a20b2fe535bf2b54f84a8358ddc4627b8ce000
Kevin Porras <kevin.porras@pantheon.io>
2023-04-05 14:07:30 -0600

    Document updating hash_salt.

--- assets/settings.pantheon.php
+++ assets/settings.pantheon.php
@@ -129,7 +129,10 @@ if (isset($_SERVER['PRESSFLOW_SETTINGS'])) {
 /**
  * Handle Hash Salt Value from Drupal
  *
- * Issue: https://github.com/pantheon-systems/drops-8/issues/10
+ * Changing these will invalidate all one-time login links.
+ * Pantheon sets this values for you. If you want to shuffle it you could
+ * use terminus env:rotate-random-seed command:
+ * https://docs.pantheon.io/terminus/commands/env-rotate-random-seed
  *
  */
 if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
